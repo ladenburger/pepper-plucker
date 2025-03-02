9f5ac1e915f1e9140f984899d4b5b6466ea645d9
ladenburger <b.ladenburger@protonmail.com>
2025-03-02 21:16:36 +0100

    add localized description for fruit and fruit_type

--- src/main.rs
+++ src/main.rs
@@ -61,13 +61,15 @@ struct NewFruit {
 
     /// Average weight of a single fruit in grams.
     average_weight_in_grams: f64,
+
+    fruit_descriptions: Option<HashMap<String, String>>,
+    fruit_type_descriptions: Option<HashMap<String, String>>,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct NewPlant {
     planted: NaiveDate,
-    descriptions: Option<HashMap<String, String>>,
     fruit: i32,
 }
 
@@ -90,6 +92,10 @@ async fn insert_fruit(pool: web::Data<PgPool>, new_fruit: web::Json<NewFruit>) -
     if new_fruit.new_fruit_type.is_some() && new_fruit.existing_fruit_type.is_some() {
         return HttpResponse::BadRequest()
             .body("Cannot provide both new_fruit_type and existing_fruit_type.");
+    }
+
+    if new_fruit.existing_fruit_type.is_some() && new_fruit.fruit_type_descriptions.is_some() {
+        return HttpResponse::BadRequest().body("Cannot add description to existing fruit_type");
     }
 
     let mut transaction = pool.begin().await.unwrap();
@@ -158,6 +164,32 @@ async fn insert_fruit(pool: web::Data<PgPool>, new_fruit: web::Json<NewFruit>) -
         match fruit_type_result {
             Ok(record) => {
                 fruit_type_id = record.type_id;
+                match &new_fruit.fruit_type_descriptions {
+                    Some(desc) => {
+                        for (lang_code, value) in desc {
+                            let insert_result = sqlx::query!(
+                                "INSERT INTO locale_fruit_type_desc (locale_id, fruit_type, value) VALUES ($1, $2, $3)",
+                                lang_code,
+                                fruit_type_id,
+                                value
+                            )
+                            .execute(transaction.as_mut())
+                            .await;
+
+                            match insert_result {
+                                Ok(..) => (),
+                                Err(e) => {
+                                    transaction.rollback().await.unwrap();
+
+                                    return HttpResponse::InternalServerError().body(
+                                        "Error creating description. ".to_owned() + &e.to_string(),
+                                    );
+                                }
+                            }
+                        }
+                    }
+                    None => (),
+                };
             }
             Err(e) => {
                 transaction.rollback().await.unwrap();
@@ -198,49 +230,25 @@ async fn insert_fruit(pool: web::Data<PgPool>, new_fruit: web::Json<NewFruit>) -
     .fetch_one(transaction.as_mut())
     .await;
 
-    match fruit_result {
-        Ok(_) => {
-            transaction.commit().await.unwrap();
-            HttpResponse::Created().finish()
-        }
+    let fruit_id: i32 = match fruit_result {
+        Ok(res) => res.fruit_id,
         Err(e) => {
             transaction.rollback().await.unwrap();
 
-            HttpResponse::InternalServerError().body(String::from(
+            return HttpResponse::InternalServerError().body(String::from(
                 "Failed to create fruit: Database insert operation failed. ".to_owned()
                     + &e.to_string(),
-            ))
-        }
-    }
-}
-
-async fn insert_plant(pool: web::Data<PgPool>, new_plant: web::Json<NewPlant>) -> impl Responder {
-    let mut transaction = pool.begin().await.unwrap();
-
-    let query_result = sqlx::query!(
-        "insert into plant (planted, fruit) values ($1, $2) returning plant_id",
-        new_plant.planted,
-        new_plant.fruit
-    )
-    .fetch_one(transaction.as_mut())
-    .await;
-
-    let plant_id: String = match query_result {
-        Ok(r) => r.plant_id,
-        Err(e) => {
-            transaction.rollback().await.unwrap();
-
-            return HttpResponse::InternalServerError()
-                .body("Error creating plant. ".to_owned() + &e.to_string());
+            ));
         }
     };
-    match &new_plant.descriptions {
+
+    match &new_fruit.fruit_descriptions {
         Some(desc) => {
             for (lang_code, value) in desc {
                 let insert_result = sqlx::query!(
                     "INSERT INTO locale_fruit_desc (locale_id, fruit, value) VALUES ($1, $2, $3)",
                     lang_code,
-                    new_plant.fruit,
+                    fruit_id,
                     value
                 )
                 .execute(transaction.as_mut())
@@ -261,6 +269,26 @@ async fn insert_plant(pool: web::Data<PgPool>, new_plant: web::Json<NewPlant>) -
     };
 
     transaction.commit().await.unwrap();
+
+    HttpResponse::Ok().body("")
+}
+
+async fn insert_plant(pool: web::Data<PgPool>, new_plant: web::Json<NewPlant>) -> impl Responder {
+    let query_result = sqlx::query!(
+        "insert into plant (planted, fruit) values ($1, $2) returning plant_id",
+        new_plant.planted,
+        new_plant.fruit
+    )
+    .fetch_one(pool.as_ref())
+    .await;
+
+    let plant_id: String = match query_result {
+        Ok(r) => r.plant_id,
+        Err(e) => {
+            return HttpResponse::InternalServerError()
+                .body("Error creating plant. ".to_owned() + &e.to_string());
+        }
+    };
 
     return HttpResponse::Ok().body(plant_id);
 }
